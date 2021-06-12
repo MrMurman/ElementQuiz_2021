716865dce55c9df6612fe72a926840cd1b8ccb27
MrMurman <storm2150@yandex.ru>
2021-06-12 22:08:41 +0300

    v 0.8 -- midway through Tutorial

--- ElementQuiz_2021/ViewController.swift
+++ ElementQuiz_2021/ViewController.swift
@@ -7,13 +7,115 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum Mode {
+    case flashCard
+    case quiz
+}
 
+enum State {
+    case question
+    case answer
+}
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    @IBOutlet weak var modeSelector: UISegmentedControl!
+    @IBOutlet weak var textField: UITextField!
+    
+    
+    let elementList = ["Carbon", "Gold", "Chlorine", "Sodium"]
+    var currentElementIndex = 0
+    
+    var mode: Mode = .flashCard
+    
+    var state: State = .question
+    
+    //Quiz-specific state
+    var answerIsCorrect = false
+    var correctAnswerCount = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI()
     }
 
+    func updateFlashCardUI() {
+        let elementName = elementList[currentElementIndex]
+        let image = UIImage(named: elementName)
+        imageView.image = image
+        
+        if state == .answer {
+            answerLabel.text = elementName
+        } else {
+            answerLabel.text = "?"
+        }
+    }
 
+    func updateQuizUI() {
+        switch state {
+        case .question:
+            answerLabel.text = ""
+        case .answer:
+            if answerIsCorrect {
+                answerLabel.text = "Correct!"
+            } else {
+                answerLabel.text = "❌"
+            }
+        }
+    }
+    
+    func updateUI() {
+        switch mode {
+        case .flashCard:
+            updateFlashCardUI()
+        case .quiz:
+            updateQuizUI()
+        }
+    }
+    
+    //Runs after the user hits the Return key on the keyboard
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Get the text from the text field
+        let textFieldContents = textField.text!
+        
+        // Determine whether the user answered correctly and update appropriate quiz state
+        if textFieldContents.lowercased() == elementList[currentElementIndex].lowercased() {
+            answerIsCorrect = true
+            correctAnswerCount += 1
+        } else {
+            answerIsCorrect = false
+        }
+        
+        // The app should now display the answer to the user
+        state = .answer
+        
+        updateUI()
+        
+        if answerIsCorrect {
+            print("Correct!")
+        } else {print("❌")}
+        
+        return true
+    }
+    
+    @IBAction func showAnswer(_ sender: UIButton) {
+        state = .answer
+        updateUI()
+    }
+    
+    @IBAction func next(_ sender: UIButton) {
+        currentElementIndex += 1
+        if currentElementIndex >= elementList.count {
+            currentElementIndex = 0
+        }
+        
+        state = .question
+        
+        updateUI()
+    }
 }
 
